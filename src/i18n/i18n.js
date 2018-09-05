96ab54a918c141ed9a6ff4cec2a8910cceab2e84
nirui <nirui3168@163.com>
2018-09-06 01:12:31 +0800

    注册页面

--- src/i18n/i18n.js
+++ src/i18n/i18n.js
@@ -1,29 +1,53 @@
 module.exports = {
     en: {
         message: {
-            title: 'Demo Page',
+            title: {
+                regist: "Free Regist"
+            },
             btn: {
-                regist: 'Free Regist',
+                regist: "Free Regist",
                 login: "Log in",
                 announcements: "Announcements",
-                whitePaper: "White Paper"
+                whitePaper: "White Paper",
+                registBtn: "Regist"
             },
             placeholder: {
-                mail: 'Enter your email address'
+                mail: "Enter your email address",
+                registMail: "E-mail",
+                registPassd: "Password",
+                registPassdConf: "Confirm Password",
+                registRecommend: "Recommend ID"
+            },
+            content: {
+                registText1: '已经注册?',
+                registText2: '如有疑问请联系客服：',
+                registLogin: '立即登录'
             }
         }
     },
     zh: {
         message: {
-            title: '演示页面',
+            title: {
+                regist: "免费注册"
+            },
             btn: {
-                regist: '免费注册',
-                login: '登录',
+                regist: "免费注册",
+                login: "登录",
                 announcements: "公告中心",
-                whitePaper: "白皮书"
+                whitePaper: "白皮书",
+                registBtn: "注册"
             },
             placeholder: {
-                mail: "输入邮箱地址"
+                mail: "输入邮箱地址",
+                registMail: "邮箱",
+                registPassd: "密码",
+                registPassdConf: "确认密码",
+                registRecommend: "推荐人ID（选填）"
+            },
+            content: {
+                registText1: '已经注册?',
+                registText2: '如有疑问请联系客服：',
+                registLogin: '立即登录'
             }
         }
     }
--- src/pages/RegistPage.vue
+++ src/pages/RegistPage.vue
@@ -1,19 +1,147 @@
 <template>
     <div class="regist-page">
-        <v-header :show-regist="false" />
+        <v-header class="regist-page-header" :show-regist="false" />
+        <div class="regist-page-content">
+            <div class="regist-page-content-title">{{ $t('message.title.regist') }}</div>
+            <div class="regist-page-content-form">
+                <b-input
+                    v-model="registData.email"
+                    class="regist-page-content-form-input"
+                    icon="email_icon"
+                    :placeholder="$t('message.placeholder.registMail')"
+                    :verify="emailVerify"/>
+                <b-input
+                    v-model="registData.passd"
+                    class="regist-page-content-form-input regist-page-gap-top_20"
+                    icon="password_icon"
+                    type="password"
+                    :placeholder="$t('message.placeholder.registPassd')"/>
+                <b-input
+                    v-model="registData.passdConf"
+                    class="regist-page-content-form-input regist-page-gap-top_20"
+                    icon="password_icon"
+                    type="password"
+                    :placeholder="$t('message.placeholder.registPassdConf')"/>
+                <b-input
+                    v-model="registData.recommend"
+                    class="regist-page-content-form-input regist-page-gap-top_20"
+                    icon="referral_icon"
+                    :placeholder="$t('message.placeholder.registRecommend')"/>
+            </div>
+            <b-button
+                class="regist-page-content-btn"
+                :class="{'enabled': registEnabled}"
+                active="regist-page-content-btn-active"
+                :disabled="!registEnabled">{{ $t('message.btn.registBtn') }}</b-button>
+            <div class="regist-page-content-footer regist-page-gap-top_20">
+                <div>
+                    {{ $t('message.content.registText1') }}
+                    <router-link class="regist-page-content-footer-login" to="/demopage">{{ $t('message.content.registLogin') }}</router-link>
+                </div>
+                <div class="regist-page-gap-top_10">
+                    {{ $t('message.content.registText2') }}
+                    {{ contact }}
+                </div>
+            </div>
+        </div>
     </div>
 </template>
 
 <script>
 import Header from '@/components/Header/Header';
+import Input from '@/components/Input/Input';
+import Button from '@/components/Button/Button'
 
 export default {
+    data() {
+        return {
+            contact: 'support@bitmax.io',
+            registEnabled: true,
+            registData: {
+                email: '',
+                passd: '',
+                passdConf: '',
+                recommend: ''
+            }
+        }
+    },
+    methods: {
+        emailVerify(data) {
+            if (/^([a-zA-Z0-9_\.\-])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/.test(data)) {
+                return true;
+            }
+            return false;
+        }
+    },
     components: {
-        'v-header': Header
+        'v-header': Header,
+        'b-input': Input,
+        'b-button': Button
     }
 }
 </script>
 
 <style lang="scss" scoped>
+@import '../style/base';
+@import '../style/commom';
+.regist-page {
+    height: 100%;
+    width: 100%;
+    display: flex;
+    flex-direction: column;
 
+    &-header {
+        width: 100%;
+        flex-grow: 0;
+        z-index: 2;
+    }
+
+    &-content {
+        width: 100%;
+        flex-grow: 1;
+        background-color: $c-white;
+        padding: px2rem(30px) px2rem(20px);
+        &-title {
+            @include font-dpr(30px);
+            font-weight: 300;
+        }
+        &-form {
+            margin: px2rem(30px) 0 px2rem(20px);
+            &-input {
+                @include font-dpr(14px);
+            }
+        }
+        &-btn {
+            @include ta_c;
+            @include font-dpr(16px);
+            color: $c-white;
+            width: 100%;
+            height: px2rem(42px);
+            line-height: px2rem(42px);
+            background-color: #172B4D;
+            opacity: .2;
+            &-active {
+                opacity: .7!important;
+            }
+            &.enabled {
+                opacity: 1;
+                background-color: #006CE1;
+            }
+        }
+        &-footer {
+            @include font-dpr(12px);
+            color: #A8ACB9;
+            &-login {
+                color: #006CE1;
+            }
+        }
+    }
+
+    &-gap-top_10 {
+        margin-top: px2rem(10px);
+    }
+    &-gap-top_20 {
+        margin-top: px2rem(20px);
+    }
+}
 </style>
