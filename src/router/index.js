08ec685b41ec741ee09b4adb88ee3d7fe21cedbc
chenhui <chenhui@haizhi.com>
2018-09-05 15:59:29 +0800

    router

--- src/router/index.js
+++ src/router/index.js
@@ -9,6 +9,10 @@ export default new Router({
     routes: [
         {
             path: '/',
+            redirect: 'Home'
+        },
+        {
+            path: '/Home',
             name: 'Home',
             component: Home
         },
